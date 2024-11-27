b63c2b6fcf001a511b708bc51488e5919957b938
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-11-26 23:36:11 -0500

    feat: integrate NewsBloc into main app and provide it via BlocProvider

--- newsly/lib/main.dart
+++ newsly/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:newsly/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:newsly/presentation/bloc/news_bloc.dart';
+import 'package:newsly/presentation/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomeScreen());
+    return BlocProvider(
+      create: (context) => NewsBloc(),
+      child: const MaterialApp(
+          debugShowCheckedModeBanner: false, home: HomeScreen()),
+    );
   }
 }
